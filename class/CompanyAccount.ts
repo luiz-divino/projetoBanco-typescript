a2f99367d14c3eefa5074078259498776dbac0e2
unknown <nandodivinodev@gmail.com>
2025-12-23 16:58:38 -0300

    criei o metodo getLoan(emprestimo) acessando e acessei o atributo status da conta para fazer a verificação de conta ativa e o atributo balance para atualizar o valor da conta

--- class/CompanyAccount.ts
+++ class/CompanyAccount.ts
@@ -1,12 +1,18 @@
 import { DioAccount } from "./DioAccount"
 
-export class CompanyAccount extends DioAccount {
 
+export class CompanyAccount extends DioAccount {
   constructor(name: string, accountNumber: number){
     super(name, accountNumber)
   }
 
-  getLoan = (): void => {
-    console.log('Voce pegou um empréstimo')
+  getLoan = (loanAmount: number): void => {
+    if(this.validateStatus()) {
+      this.balance += loanAmount
+      console.log(`Empréstimo de ${loanAmount} concedido com sucesso!`)
+      console.log(`Seu saldo atual é ${this.balance}`)
+    } else {
+      console.log('Conta inválida')
+    }
   }
 }
